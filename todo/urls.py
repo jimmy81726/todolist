15ead0f97cb1b03fee3e79f9b81c69f5624345fb
jerry <17app001@gmail.com>
2023-05-09 21:47:00 +0800

    新增todo功能

--- todo/urls.py
+++ todo/urls.py
@@ -18,9 +18,5 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [   
-    path('register/',views.user_register,name='register'),   
-    path('login/',views.user_login,name='login'),
-    path('logout/',views.user_logout,name='logout'),
-    path('profile/',views.user_profile,name='profile'),   
-
+    path('',views.todo,name='todo'),    
 ]
